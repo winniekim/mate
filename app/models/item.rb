9ddad891a65d9e51007840b5a3cac3723ce66d55
Ubuntu <ubuntu@ip-172-31-44-216.ap-northeast-1.compute.internal>
2019-08-23 17:53:43 +0000

    photo show subject and content

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   enum category: [:museum, :exhibition, :farm, :musical, :natural, :temple, :science, :others]
-  enum area: [ :seoul, :gg, :gw, :cb, :cn, :gb, :gn, :jb, :jn, :jeju ]
+  enum area: [ :Seoul, :Daejeon, :Daegu, :Busan, :Incheon, :Pohang, :Ulsan, :Jeju, :Chuncheon, :Suwon ]
 
   def self.generate_seed
     items = ['블루베리 체험농장', '뮤지컬', 'VR 체험관', '어린이 박물관', '낭만 미술관', '갯벌 체험']
